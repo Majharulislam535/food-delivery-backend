b338524bfb40665f1f4f6089d0c5aaef1366f9e6
pixelweb <pixelweb535@gmail.com>
2022-01-24 20:32:23 +0600

    4th commit

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const port = process.env.PORT || 5000;
 const cors = require("cors");
 require("dotenv").config();
 
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 app.use(cors());
 app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qazkn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 app.get('/', (req, res) => {
